75fb405636740a9546c1837ffdfd7c64f64dffae
Pedro-RGS <pedro.gsouza@upe.br>
2025-07-10 20:39:01 -0300

    feat: update of the seed.ts to seed question table

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -9,10 +9,13 @@ await seed(db, schema).refine((f) => {
       count: 20,
       columns: {
         name: f.companyName(),
-        description: f.loremIpsum()
-      }
-    }
-  }
+        description: f.loremIpsum(),
+      },
+    },
+    questions: {
+      count: 20,
+    },
+  };
 });
 
 await dbConnection.end();
