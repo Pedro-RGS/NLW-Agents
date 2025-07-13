37f31cbd03a29b43a627116725d14987b65c051a
Pedro-RGS <pedro.gsouza@upe.br>
2025-07-13 17:33:29 -0300

    feat: new route created to upload audios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
 import {
   serializerCompiler,
   validatorCompiler,
@@ -12,6 +13,7 @@ import {
 import { createRoomsRoute } from "./routes/createRooms.ts";
 import { getRoomQuestionsRoute } from "./routes/getRoomQuestions.ts";
 import { getRoomsRoute } from "./routes/getRooms.ts";
+import { uploadAudioRoute } from "./routes/uploadAudio.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -26,9 +28,11 @@ app.get("/health", () => {
   return "OK";
 });
 
+app.register(fastifyMultipart);
 app.register(getRoomsRoute);
 app.register(createRoomsRoute);
 app.register(getRoomQuestionsRoute);
 app.register(createRoomQuestionsRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
